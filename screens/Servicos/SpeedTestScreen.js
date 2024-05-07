097d159b47b072e6266fca520dc07443540051c4
murilo-henrique060 <henrique.henryconde@gmail.com>
2024-05-07 10:37:03 -0300

    chore: Update SpeedTestScreen component styles
    Update the styles of the SpeedTestScreen component to use a custom WebView style with a background color of '#151526'.

--- screens/Servicos/SpeedTestScreen.js
+++ screens/Servicos/SpeedTestScreen.js
@@ -1,15 +1,17 @@
+// React Packages
+import { StyleSheet } from 'react-native';
 import { WebView } from 'react-native-webview';
 
-const SpeedTestScreen = () => {
+// Main Component
+export default function SpeedTestScreen() {
   return (
-    <WebView
-      source={{ uri: 'https://www.speedtest.net/pt' }}
-      originWhitelist={['https://www.speedtest.net/pt']}
-      setBuiltInZoomControls={false}
-      thirdPartyCookiesEnabled={false}
-      style={{ backgroundColor: '#151526' }}
-    />
+    <WebView style={styles.webView} source={{ uri: 'https://www.speedtest.net/pt' }} setBuiltInZoomControls={false}/>
   )
 }
 
-export default SpeedTestScreen;
+// Styles
+const styles = StyleSheet.create({
+  webView: {
+    backgroundColor: '#151526',
+  }
+});
